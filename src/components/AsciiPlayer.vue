789f8ce4bb8d6b13b2871eff7d3341a1f84c2545
Brian Fryer <brianfryer@gmail.com>
2019-07-20 18:19:41 -0500

    Adding ascciiplayer component.

---
+++ src/components/AsciiPlayer.vue
@@ -0,0 +1,23 @@
+<template>
+  <figure :id="id"></figure>
+</template>
+
+<script>
+  export default {
+    props: ['id'],
+    data() {
+      return {
+        source: `https://asciinema.org/a/${this.id}.js`,
+        ascid: `asciicast-${this.id}`
+      }
+    },
+    mounted() {
+      // Can't use script tags directly apparently
+      let asciiscript = document.createElement('script');
+      asciiscript.setAttribute('src', this.source);
+      asciiscript.setAttribute('id', this.ascid);
+      console.log(this.id);
+      document.getElementById(this.id).appendChild(asciiscript)
+    }
+  }
+</script>
