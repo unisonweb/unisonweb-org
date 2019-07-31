99755a92d53ec9b8d50fbfb10110ebc921decf24
Brian Fryer <brianfryer@gmail.com>
2019-07-31 17:46:03 -0500

    Getting started with sidebar.

--- src/components/DocSidebar.vue
+++ src/components/DocSidebar.vue
@@ -4,9 +4,14 @@
     <ul class="uc-doc-sidebar__links">
       <li v-for="(link, i) in links" :key="i">
         <g-link
-          :to="link.url">
+          :to="link.path">
           {{link.label}}
         </g-link>
+        <ul>
+          <li v-for="(heading, j) in link.headings" :key="j">
+            {{heading.value}}
+          </li>
+        </ul>
       </li>
     </ul>
 
@@ -14,10 +19,45 @@
 </template>
 
 <script>
+  import find from 'lodash.find'
+  import docLinks from '~/data/doc-links.yml'
+
   export default {
     props: {
-      links: { type: Array, defaul: null },
+      currentDocSubtitles: { type: Array, default: null },
     },
+    computed: {
+      links() {
+        const vm = this
+
+        const links = docLinks.map(link => {
+          const page = find(vm.allDocPage, (p) => {
+            return p.path === vm.$router.currentRoute.path
+          })
+
+          return {
+            label: link.label,
+            path: link.path,
+            headings: page.headings,
+            subheadings: page.subheadings,
+          }
+        })
+
+        return links
+      },
+      allDocPage() {
+        const vm = this
+
+        return vm.$static.allDocPage.edges.map(edge => {
+          return edge.node
+        })
+      },
+    },
+    mounted() {
+      // console.log(this.$router.currentRoute)
+      // console.log(this.currentDocPage)
+      // console.log(this.$static)
+    }
   }
 </script>
 
@@ -25,8 +65,14 @@
 
   .uc-doc-sidebar {
 
+    @include max-screen(breakpoint(xs, max)) {
+      display: none;
+    }
+
     @include min-screen(breakpoint(md)) {
-      flex: 0 0 auto;
+      position: absolute;
+      top: 0;
+      left: 0;
       width: (3/12 * 100%);
     }
   }
@@ -58,3 +104,24 @@
   }
 
 </style>
+
+<static-query>
+  query {
+    allDocPage {
+      edges {
+        node {
+          title
+          headings (depth: h2) {
+            value
+            anchor
+          }
+          subheadings: headings (depth: h3) {
+            value
+            anchor
+          }
+          path
+        }
+      }
+    }
+  }
+</static-query>
