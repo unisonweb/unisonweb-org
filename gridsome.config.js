6b79c63c0f22101867370a27158a577d11aebdfc
Brian Fryer <brianfryer@gmail.com>
2019-09-09 14:42:01 -0500

    Disabling development mode.

--- gridsome.config.js
+++ gridsome.config.js
@@ -108,7 +108,7 @@ module.exports = {
 
   chainWebpack: config => {
 
-    config.mode('development')
+    // config.mode('development')
 
     const svgRule = config.module.rule('svg')
     svgRule.uses.clear()
