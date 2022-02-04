ce952fd3029f9e1ca3d94d390c3e6971a46af917
Simon Højberg <r.hackr@gmail.com>
2022-02-04 16:30:11 -0500

    Make smaller post column

--- src/layouts/Blog.vue
+++ src/layouts/Blog.vue
@@ -3,7 +3,7 @@
     <un-page-section>
       <div class="container">
         <div class="row center-xs u-align--left">
-          <div class="col-xs-12 col-sm-9 col-md-10">
+          <div class="col-xs-12 col-sm-9 col-md-8">
 
             <slot />
 
